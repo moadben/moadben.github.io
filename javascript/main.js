258829f7413ba9faa2b2fc16a021bd16bf23fc2b
Moad Ben-Suleiman <moadben95@gmail.com>
2016-02-27 16:08:36 -0800

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,44 @@
+window.onload = function onLoad() {
+    var circle = new ProgressBar.Circle('#PHP', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+     var circle_2 = new ProgressBar.Circle('#C', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+      var circle_3 = new ProgressBar.Circle('#Java', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+      var circle_4 = new ProgressBar.Circle('#Python', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+       var circle_5 = new ProgressBar.Circle('#HTML', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+        var circle_6 = new ProgressBar.Circle('#CSS', {
+        color: '#FCB03C',
+        duration: 3000,
+        easing: 'easeInOut'
+    });
+
+    circle.animate(1);
+    circle_2.animate(1);
+    circle_3.animate(1);
+    circle_4.animate(1);
+    circle_5.animate(1);
+    circle_6.animate(1);
+};
